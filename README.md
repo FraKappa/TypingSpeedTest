8e62e5c00809a26774901c718ead7db374bade44
Francesco Cappetti <f.cappetti.05@gmail.com>
2021-03-26 17:15:31 +0100

    Preparing to release

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# TypingSpeedTest
+# pytype
 A simple Typing Speed Test right in your terminal made with Python and the curses module.
 
 ![pytype](https://user-images.githubusercontent.com/72104527/112655651-e7a7e500-8e50-11eb-9b46-ecb8df2e10e4.gif)
+
+## Installation
+You can install pytype by simply typing in your terminal:
+```
+pip install pytype
+```
+
+To run the program:
+```
+pytype
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name='pytype',
+    packages=['pytype'],
+    version='0.1',
+    license='MIT',
+    description='A simple Typing Speed Test right in your terminal made with Python and the curses module.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Kappa',
+    author_email='f.cappetti.05@gmail.com',
+    url='https://github.com/FraKappa/pychordpro',
+    download_url='https://github.com/FraKappa/pychordpro/archive/v_01.tar.gz',
+    keywords=['typing'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+  ]
+)
